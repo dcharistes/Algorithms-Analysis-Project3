28081c5601f82b7292974fca730af352c15aa0b3
dcharistes <d.charistes@gmail.com>
2025-05-18 23:53:30 +0300

    re added graph funcs in task1 task2.c

--- task1.c
+++ task1.c
@@ -1,8 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#include "./graph_funcs/prim_graph_funcs.h"
+//#include "./graph_funcs/prim_graph_funcs.h"
 
+typedef struct Node {
+    int dest;
+    int cost;
+    struct Node* next;
+} Node;
+
+typedef struct {
+    int n;
+    Node** adjList;
+} Graph;
+
+Node* createNode(int dest, int cost) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->dest = dest;
+    newNode->cost = cost;
+    newNode->next = NULL;
+    return newNode;
+}
+
+Graph* createGraph(int nodeCount) {
+    Graph* graph = (Graph*)malloc(sizeof(Graph));
+    graph->n = nodeCount;
+    graph->adjList = (Node**)malloc(nodeCount * sizeof(Node*));
+    for (int i = 0; i < nodeCount; i++) {
+        graph->adjList[i] = NULL;
+    }
+    return graph;
+}
+
+void addEdge(Graph* graph, int src, int dest, int cost) {
+    Node* newNode = createNode(dest, cost);
+    newNode->next = graph->adjList[src];
+    graph->adjList[src] = newNode;
+}
+
+void displayEdges(Graph* graph, int* totalCost) {
+    *totalCost = 0;
+    for (int i = 0; i < graph->n; i++) {
+        Node* temp = graph->adjList[i];
+        while (temp) {
+            if (i < temp->dest) { // avoid duplicate printing
+                printf("(%d)---(%d|%d)\n", i, temp->dest, temp->cost);
+                *totalCost += temp->cost;
+            }
+            temp = temp->next;
+        }
+    }
+}
 
 Graph* primsMST(Graph* g, int start);
 int main() {
--- task2.c
+++ task2.c
@@ -1,6 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "./graph_funcs/kruskal_graph_funcs.h"
+//#include "./graph_funcs/kruskal_graph_funcs.h"
+
+#define MAX_NODES 100
+
+typedef struct Node {
+    int dest;
+    int weight;
+    struct Node* next;
+} Node;
+
+typedef struct {
+    int u, v, weight;
+} Edge;
+
+typedef struct {
+    int V;
+    Node* adjList[MAX_NODES];
+} Graph;
+
+void initGraph(Graph* g, int vertices) {
+    g->V = vertices;
+    for (int i = 0; i < MAX_NODES; i++) {
+        g->adjList[i] = NULL;
+    }
+}
+
+Node* createNode(int dest, int weight) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->dest = dest;
+    newNode->weight = weight;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void addEdge(Graph* g, int u, int v, int weight) {
+    Node* newNode = createNode(v, weight);
+    newNode->next = g->adjList[u];
+    g->adjList[u] = newNode;
+}
+
+int countEdges(Graph* g) {
+    int count = 0;
+    for (int i = 0; i < g->V; i++) {
+        Node* curr = g->adjList[i];
+        while (curr) {
+            count++;
+            curr = curr->next;
+        }
+    }
+    return count;
+}
+
+void extractEdges(Graph* g, Edge* edgeList, int* edgeCount) {
+    *edgeCount = 0;
+    for (int i = 0; i < g->V; i++) {
+        Node* curr = g->adjList[i];
+        while (curr) {
+            if (i < curr->dest) { // avoid duplicates
+                edgeList[*edgeCount].u = i;
+                edgeList[*edgeCount].v = curr->dest;
+                edgeList[*edgeCount].weight = curr->weight;
+                (*edgeCount)++;
+            }
+            curr = curr->next;
+        }
+    }
+}
+
+void sortEdges(Edge* edges, int count) {
+    for (int i = 0; i < count - 1; i++) {
+        for (int j = i + 1; j < count; j++) {
+            if (edges[i].weight > edges[j].weight) {
+                Edge temp = edges[i];
+                edges[i] = edges[j];
+                edges[j] = temp;
+            }
+        }
+    }
+}
+
+int find(int* parent, int v) {
+    if (parent[v] == v)
+        return v;
+    return find(parent, parent[v]);
+}
 
 
 int unionSet(int* parent, int u, int v) {
